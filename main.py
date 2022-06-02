c96097abbf8ba22ff6d7cf1f6437778e6be4dcd0
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-02 10:18:23 +0530

    init capture script

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
 
 if __name__ == "__main__":
-    # do remaining
-    pass
+    import pyshark
+
+    iface_name = 'wlp2s0'
+    filter_string = 'tcp port 80'
+
+    capture = pyshark.LiveCapture(
+        interface=iface_name,
+        bpf_filter=filter_string
+    )
+    capture.sniff(timeout=1)
+    if len(capture) > 0:
+        for packet in capture:
+            print('source: ' + packet.ip.src)
+
