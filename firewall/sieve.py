9e978a2889eeb7ad5e5f4f730dbd6830b8962319
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-10 13:08:18 +0530

    init Firewall class

--- firewall/sieve.py
+++ firewall/sieve.py
@@ -1,9 +1,19 @@
-def firewall(clf_model, packet_info) -> None:
-    """
+class Firewall:
+    def __init__(self, clf_model, packet_info) -> None:
+        """
 
-    :param clf_model: sklearn model instance
-    :param packet_info: dictionary containing all attrs required fir classification
-    :return: None
-    """
+        :param clf_model: sklearn model instance
+        :param packet_info: dictionary containing all attrs required fir classification
+        :return: None
+        """
+        self.__clf_model = clf_model
 
+        self.packet_info = packet_info
+
+    def filter(self) -> None:
+        """
+        triggers clf funcs and blacklisting procedures
+        :return: None
+        """
+        
 
