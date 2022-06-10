27f5325de8f284e30bcd4a94bce22163b19b1cac
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-10 13:04:26 +0530

    added constants

---
+++ firewall/constants.py
@@ -0,0 +1,17 @@
+CATEGORICAL_COLS = [
+    'Protocol',
+    'Inbound'
+]
+LABEL_COLS = [
+    'Label'
+]
+NUMERICAL_COLS = [
+    'URG Flag Count', 'Fwd PSH Flags',
+    'RST Flag Count', 'CWE Flag Count', 'Packet Length Std',
+    'Init_Win_bytes_forward', 'Fwd Packet Length Std', 'Active Mean',
+    'Idle Mean', 'Avg Fwd Segment Size',
+    'Fwd Packet Length Mean', 'Fwd Packets/s', 'Packet Length Mean',
+    'Average Packet Size', 'Fwd IAT Min', 'Flow Duration',
+    'Fwd IAT Total', 'Active Std', 'SYN Flag Count', 'Fwd IAT Std',
+    'Flow IAT Std', 'Subflow Fwd Bytes', 'Total Length of Fwd Packets'
+]
