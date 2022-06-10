ab4782967fcdebac9eb89df85c188b05c6201c91
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-10 09:22:09 +0530

    copied sniffer content to main

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
+from scapy.sendrecv import AsyncSniffer
+
+from custom_session import generate_session_class
 
 
-if __name__ == "__main__":
-    import pyshark
+def create_sniffer(input_interface, sys_dst_ip, cb=None):
+    assert (input_interface is None)
 
-    iface_name = 'wlp2s0'
-    filter_string = 'tcp port 80'
+    custom_session = generate_session_class(sys_dst_ip)
 
-    capture = pyshark.LiveCapture(
-        interface=iface_name,
-        bpf_filter=filter_string
+    return AsyncSniffer(
+        iface=input_interface,
+        filter="ip and (tcp or udp)",
+        # prn=cb,
+        session=custom_session,
+        store=False,
+        count=0
     )
-    capture.sniff(timeout=1)
-    if len(capture) > 0:
-        for packet in capture:
-            print('source: ' + packet.ip.src)
 
+
+# TODO: this should be replaced with different func name
+def main():
+    input_interface = None
+    sys_dst_ip = None
+
+    # TEST: this is a simple callback function
+    def simple_cb(foo, bar):
+        print(f"Simple CB happened. Args: {foo}, {bar}")
+        pass
+
+    sniffer = create_sniffer(
+        # cb=simple_cb,
+        input_interface=input_interface,
+        sys_dst_ip=sys_dst_ip
+    )
+
+    sniffer.start()
+
+    try:
+        sniffer.join()
+    except KeyboardInterrupt:
+        sniffer.stop()
+    finally:
+        sniffer.join()
+
+
+# TODO: should be removed
+if __name__ == "__main__":
+    main()
