8c35b8a0683ae6aae352cbd3198d525511f649c6
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-10 14:55:49 +0530

    correcte dmodel path

--- firewall/utils/model_utils.py
+++ firewall/utils/model_utils.py
@@ -10,7 +10,7 @@ def load_clf_model(model_name):
     :param model_name: name of model present in /models without the .pkl extension
     :return: sklearn model instance
     """
-    with open(os.path.join("..", "models", f"{model_name}.pkl"), 'rb') as f:
+    with open(os.path.join("models", f"{model_name}.pkl"), 'rb') as f:
         return pkl.load(f)
 
 
