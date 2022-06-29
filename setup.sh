c7b04f5a0f96f3ae9fba4ef34dcbd3a2a4e410da
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-30 02:38:15 +0530

    added setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,26 @@
 #! /bin/bash
+set -xe
+
+PROJECT_ROOT="/root/armour"
+PROJECT_NAME="armour"
+DB_ROOT="/var/local/${PROJECT_NAME}"
 
 echo "initialing setup..."
 sudo apt update -y && sudo apt upgrade -y
 sleep 2m
-sudo apt install tshark -y
-python3 -m pip install pyshark
 
-## check if user already a member of wireshark group
+mkdir -p $PROJECT_ROOT
+git clone https://github.com/Iam-VM/armour.git $PROJECT_ROOT
+cd $PROJECT_ROOT || exit
 
+mkvirtualenv $PROJECT_NAME
+echo "cd $PROJECT_ROOT" >> "/root/.virtualenvs/${PROJECT_NAME}/bin/postactivate"
+workon $PROJECT_NAME
+
+pip install -r requirements.txt
+
+# install and config sqlite
+apt install sqlite3 -y
+mkdir -p $DB_ROOT
+`which python` migrations/armour.sqlite.py $PROJECT_NAME && echo "Migrations Complete"
 
