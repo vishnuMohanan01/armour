6a590061d2f42ce5cf843225e94737f58b7871af
Iam-VM <vishnumohanantheking@gmail.com>
2022-06-30 02:35:59 +0530

    added db migrate script

---
+++ migrations/armour.sqlite.py
@@ -0,0 +1,23 @@
+import os
+import sqlite3
+import sys
+
+# sys.argv[1] is project name
+DB_ROOT = f"/var/local/{sys.argv[1]}"
+DB_NAME = "armour.db"
+
+# creating db directory if not exists
+if not os.path.exists(DB_ROOT):
+    os.makedirs(DB_ROOT)
+
+# creating db file path
+DB_PATH = os.path.join(DB_ROOT, DB_NAME)
+
+con = sqlite3.connect(DB_PATH)
+cur = con.cursor()
+
+# create blacklist
+cur.execute("CREATE TABLE blacklist (address text NOT NULL)")
+
+# create whitelist
+cur.execute("CREATE TABLE whitelist (address text NOT NULL)")
