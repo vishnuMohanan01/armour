0c9da6fe1d3982d8fad7789c3e6f74866b5c70bf
Iam-VM <vishnumohanantheking@gmail.com>
2022-07-01 01:53:47 +0530

    key check using get() with no keyword arg

--- firewall/sieve.py
+++ firewall/sieve.py
@@ -43,6 +43,6 @@ class Firewall:
             if not self.packet_info['src_ip'].values[0] in self.__ip_dict.keys():
                 self.__ip_dict[self.packet_info['src_ip'].values[0]] = 1
             else:
-                if self.__ip_dict.get(self.packet_info['src_ip'].values[0], default=0) > 20:
+                if self.__ip_dict.get(self.packet_info['src_ip'].values[0], 0) > 20:
                     blacklist(self.packet_info['src_ip'].values[0])
                 self.__ip_dict[self.packet_info['src_ip'].values[0]] += 1
