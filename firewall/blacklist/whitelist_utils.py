d9ef765ba41921e10ce5752c7955669501f3e963
Iam-VM <vishnumohanantheking@gmail.com>
2022-07-01 10:05:35 +0530

    added is_ip_whitelisted func

--- firewall/blacklist/whitelist_utils.py
+++ firewall/blacklist/whitelist_utils.py
@@ -1,3 +1,12 @@
+import os
+import sqlite3
+
+DB_ROOT = "/var/local/armour"
+DB_NAME = "armour.db"
+
+DB_PATH = os.path.join(DB_ROOT, DB_NAME)
+
+
 def is_port_white_listed(port_no):
     """
     Checks if the port is whitelisted
@@ -7,4 +16,24 @@ def is_port_white_listed(port_no):
 
     if port_no == 22 or 60000 <= port_no <= 63000:
         return True
+    return False
+
+
+def is_ip_whitelisted(ip_address):
+    con = sqlite3.connect(DB_PATH)
+
+    cur = con.cursor()
+
+    cur.execute("SELECT address from whitelist;")
+
+    rows = cur.fetchall()
+    ip = []
+    for row in rows:
+        ip.append(row[0])
+
+    con.close()
+
+    if ip_address in ip:
+        return True
+
     return False
